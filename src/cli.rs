f657ad2c92dbf40a7cd931d09ca96f33f8482e9a
nnyyxxxx <nnyyxxxx@protonmail.com>
2025-01-19 09:00:52 -0500

    feat: add short flags for existing cli args

--- src/cli.rs
+++ src/cli.rs
@@ -4,15 +4,15 @@ use clap::Parser;
 #[command(author, version, about)]
 pub struct Args {
     /// Skip root user check
-    #[arg(long)]
+    #[arg(long, short = 'r')]
     pub no_root_check: bool,
 
     /// Skip terminal size check
-    #[arg(long)]
+    #[arg(long, short = 's')]
     pub no_size_check: bool,
 
     /// Enable mouse support
-    #[arg(long)]
+    #[arg(long, short = 'm')]
     pub mouse: bool,
 }
 
