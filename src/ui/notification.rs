df1631f0c865292f4525c8e897b4b4a8a2a92c00
nnyyxxxx <nnyyxxxx@protonmail.com>
2025-04-14 00:26:26 -0400

    notification: reduce dup

--- src/ui/notification.rs
+++ src/ui/notification.rs
@@ -4,32 +4,26 @@ pub fn get_notification_title(
     error_message: &Option<(String, SystemTime)>,
     copy_notification_time: Option<SystemTime>,
 ) -> String {
-    let error_msg = get_error_message(error_message);
-    if let Some(msg) = error_msg {
+    if let Some(msg) = check_notification_time(
+        error_message
+            .as_ref()
+            .map(|(msg, time)| (msg.as_str(), *time)),
+    ) {
         return msg;
     }
 
-    let copy_msg = get_copy_message(copy_notification_time);
-    if let Some(msg) = copy_msg {
+    if let Some(msg) = check_notification_time(copy_notification_time.map(|time| ("Copied!", time)))
+    {
         return msg;
     }
 
     " Auth ".to_string()
 }
 
-fn get_error_message(error_message: &Option<(String, SystemTime)>) -> Option<String> {
-    let (msg, time) = error_message.as_ref()?;
+fn check_notification_time(notification: Option<(&str, SystemTime)>) -> Option<String> {
+    let (message, time) = notification?;
     if time.elapsed().unwrap_or_default().as_secs() < 3 {
-        Some(format!(" {} ", msg))
-    } else {
-        None
-    }
-}
-
-fn get_copy_message(copy_notification_time: Option<SystemTime>) -> Option<String> {
-    let notify_time = copy_notification_time?;
-    if notify_time.elapsed().unwrap_or_default().as_secs() < 3 {
-        Some(" Copied! ".to_string())
+        Some(format!(" {} ", message))
     } else {
         None
     }
