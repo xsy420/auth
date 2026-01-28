5441410f16e7c047998461b39014a2f40f2942d5
xsy420 <xsy0420@gmail.com>
2026-01-29 01:15:56 +0800

    refactor(clippy): fix clippy unnecessary_sort_by warning

--- src/ui/file_browser.rs
+++ src/ui/file_browser.rs
@@ -113,8 +113,8 @@ impl FileBrowser {
                 }
             }
 
-            dirs.sort_by(|a, b| a.name.to_lowercase().cmp(&b.name.to_lowercase()));
-            files.sort_by(|a, b| a.name.to_lowercase().cmp(&b.name.to_lowercase()));
+            dirs.sort_by_key(|a| a.name.to_lowercase());
+            files.sort_by_key(|a| a.name.to_lowercase());
 
             self.entries.extend(dirs);
             self.entries.extend(files);
